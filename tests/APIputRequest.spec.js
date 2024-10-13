f1e36249dc02b5fe95a0d27ee9ccc7cad0c42afa
Devi <c-drmohandhas@neogov.net>
2024-10-13 10:41:43 -0400

    second commit

--- tests/APIputRequest.spec.js
+++ tests/APIputRequest.spec.js
@@ -5,7 +5,7 @@ test('Api put request',async({request}) => {
 
 //step2: send a put request(update the data) ,give the url of the api and store it in a variable
 
-const apiputresponse = await request.put("https://reqres.in/api/users/2", {
+const apiputresponse2 = await request.put("https://reqres.in/api/users/2", {
     data: {
         "name": "Veena",
         "job": "leader"
@@ -13,15 +13,15 @@ const apiputresponse = await request.put("https://reqres.in/api/users/2", {
 })
 
  // step3: verify the response for put is 200
- expect(apiputresponse.status()).toBe(200)
+ expect(apiputresponse2.status()).toBe(200)
 
  //step4: verify the resonse is having particular text
     //step4.1:create a variable and get the text from the response
     //step4.2: verify the text having certain text
 
-    const putresponsetext = await apiputresponse.text()
+    const putresponsetext = await apiputresponse2.text()
     expect(putresponsetext).toContain('Veena')
 
     //write the response in console
-    console.log(await apiputresponse.json())
+    console.log(await apiputresponse2.json())
 })
